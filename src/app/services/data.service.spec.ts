be3c51115a90bd35de122847ca2bda21730cdf61
vrijraj <vrijraj2396@gmail.com>
2023-03-24 12:56:52 +0700

    verifying test

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,32 +1,36 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { from, of } from 'rxjs';
 import { UserDetails } from '../models/user-details';
 
 import { DataService } from './data.service';
 import { userDetails } from './testing-data';
-
+import {HttpClientTestingModule,HttpTestingController} from   '@angular/common/http/testing';
+import { Inject } from '@angular/core';
 describe('DataService', () => {
   let service: DataService;
-  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let httpMock : HttpTestingController;
   beforeEach(() => {
     
     TestBed.configureTestingModule({
-      imports:[HttpClientModule]
+      imports:[HttpClientTestingModule]
     });
-    httpClientSpy = jasmine.createSpyObj('fetchProfileDetails', ['get']);
     service = TestBed.inject(DataService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
+  
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   it('should set profile details', (done: DoneFn) => {
     const expectedUserDetails:any =  userDetails;
-    httpClientSpy.get.and.returnValue(of(expectedUserDetails));
-    service.fetchProfileDetails('freeCodeCamp')
-    service.profileDetail$.subscribe({
+    service.fetchProfileDetails('freeCodeCamp');
+    const req = httpMock.expectOne('https://api.github.com/users/freeCodeCamp');
+    expect(req.request.method === 'GET');
+    req.flush(expectedUserDetails);
+
+    const sub1 = service.profileDetail$.subscribe({
       next:(userDetails)=>{
         if(userDetails){
           expect(userDetails?.login)
@@ -34,8 +38,14 @@ describe('DataService', () => {
           .toEqual(expectedUserDetails?.login);
           done();
         }
+      },
+      error:()=>{
+        fail('Error Occured')
       }
-    })  
+    }) 
+
+    sub1.unsubscribe();
+    
   });
 });
 
